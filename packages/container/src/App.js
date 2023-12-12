00aec07cb0f103f75fa6dff9ddc19719b3b7bf63
DucTuanmdt <tuanit381@gmail.com>
2023-12-12 22:31:53 +0700

    improve performance

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import {
   StylesProvider,
   createGenerateClassName,
 } from "@material-ui/core/styles";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from "./components/Header";
-import MarketingApp from "./components/MarketingApp";
-import { BrowserRouter } from "react-router-dom";
+import Progress from "./components/Progress";
+
+const MarketingAppLazy = lazy(() => import("./components/MarketingApp"));
+const AuthAppLazy = lazy(() => import("./components/AuthApp"));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: "co",
@@ -17,7 +20,12 @@ function App() {
       <BrowserRouter>
         <div>
           <Header />
-          <MarketingApp />
+          <Suspense fallback={<Progress />}>
+            <Switch>
+              <Route path="/auth" component={AuthAppLazy} />
+              <Route path="/" component={MarketingAppLazy} />
+            </Switch>
+          </Suspense>
         </div>
       </BrowserRouter>
     </StylesProvider>
---
+++ packages/container/src/components/AuthApp.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useRef } from "react";
+import { useHistory } from "react-router-dom";
+import { mount } from "auth/AuthApp";
+
+function AuthApp() {
+  const ref = useRef(null);
+  const history = useHistory();
+
+  useEffect(() => {
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        const { pathname } = history.location;
+
+        if (pathname !== nextPathname) {
+          history.push(nextPathname);
+        }
+      },
+      initialPath: history.location.pathname,
+    });
+
+    history.listen(onParentNavigate);
+  }, []);
+
+  return <div ref={ref}></div>;
+}
+
+export default AuthApp;
